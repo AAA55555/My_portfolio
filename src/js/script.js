aaa6ca2eedc0084698f7664a25525623b789ffb4
Antonio <BulletAton@yandex.ru>
2021-04-01 15:48:56 +0400

    Sending emails is configured

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,18 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
+
+  $('form').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'mailer/smart.php',
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find('input').val('');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
 });
